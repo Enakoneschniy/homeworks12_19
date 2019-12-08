e5a3eeabc898596cfc1e76624be4b831f120bb1b
julia <julianna.smirnova@gmail.com>
2019-12-08 15:17:56 +0200

    hw_01_01

---
+++ smirnova_yuliia/smirnova_yuliia_hw_01/js/app.js
@@ -0,0 +1,15 @@
+// task_01
+
+let a = 3;
+    b = 6;
+
+if (a > b) {
+    console.log(x = a + b / 2 * 4)
+} else if (a === b){
+    console.log(x = 400)
+} else if (a < b ) {
+    console.log(x = a - b + 2 / b * 4)
+}
+
+
+
