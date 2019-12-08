f08e094e65e377d1046b9e2fe8f5897639d09320
julia <julianna.smirnova@gmail.com>
2019-12-08 21:54:59 +0200

    hw_01_03

--- smirnova_yuliia/smirnova_yuliia_hw_01/js/app.js
+++ smirnova_yuliia/smirnova_yuliia_hw_01/js/app.js
@@ -12,18 +12,40 @@ if (a > b) {
 }
 
 
-//task_02
-//     task_02_01
-let coordinateX = prompt('Координата по оси X', )
-let coordinateY = prompt('Координата по оси Y', )
+// //task_02
+// //task_02_01
+// let coordinateX = prompt('Координата по оси X', );
+// let coordinateY = prompt('Координата по оси Y', );
+//
+// y = -0.5*x - 1
+// y = 0.5*x + 1
+// if (coordinateY >= 1 - coordinateX){
+//     alert("Точка попадает в область")
+// } else {
+//     alert("Точка НЕ попадает в область")
+// }
+//
+// //task_02_02
 
-// y>=-x+1
-if (coordinateY>= 1 - coordinateX){
-    alert("Точка попадает в область")
-} else {
-    alert("Точка НЕ попадает в область")
+
+//task_03
+
+let weekNumber = prompt("Введите номер дня недели", );
+    if (weekNumber == 1){
+        alert("Понедельник")
+    } else if (weekNumber == 2){
+        alert("Вторник")
+    } else if (weekNumber == 3){
+        alert("Среда")
+    } else if (weekNumber == 4){
+        alert("Четверг")
+    } else if (weekNumber == 5){
+        alert("Пятница")
+    } else if (weekNumber == 6){
+        alert("Суббота")
+    } else if (weekNumber == 7){
+        alert("Воскресенье")
+    } else {
+        alert("Такого дня недели не существует!")
 }
-
-
-
 
