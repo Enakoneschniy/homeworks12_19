00d4885d2d806845fe6cfeb92cc6d197767b5445
julia <julianna.smirnova@gmail.com>
2019-12-08 22:23:50 +0200

    hw_01_06

--- smirnova_yuliia/smirnova_yuliia_hw_01/js/app.js
+++ smirnova_yuliia/smirnova_yuliia_hw_01/js/app.js
@@ -70,3 +70,14 @@ if (userInputNumberOne > userInputNumberTwo) {
     alert('Числа равны')
 }
 
+//task_06
+let apartmentNumber = prompt('Введите номер квартиры');
+    if (apartmentNumber >= 1 && apartmentNumber <= 20) {
+        alert("Квартира находится в подъезде №1")
+    } else if (apartmentNumber >= 21 && apartmentNumber <= 48) {
+        alert("Квартира находится в подъезде №2")
+    } else if (apartmentNumber >= 48 && apartmentNumber <= 90) {
+        alert("Квартира находится в подъезде №3")
+    } else {
+        alert("В этом доме квартиры с номерами 1-90")
+    }
