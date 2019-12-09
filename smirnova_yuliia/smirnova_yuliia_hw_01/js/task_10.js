8ed97b7a8bdd3a77898848ad63aac7a9de01d41e
julia <julianna.smirnova@gmail.com>
2019-12-10 00:18:31 +0200

    task 10 fixed

--- smirnova_yuliia/smirnova_yuliia_hw_01/js/task_10.js
+++ smirnova_yuliia/smirnova_yuliia_hw_01/js/task_10.js
@@ -4,8 +4,8 @@ let itemAmount = prompt("Введите число предметов у вас 
 let lastNumber = itemAmount[itemAmount.length - 1];
 if (lastNumber == 1) {
     alert(`У вас в корзине ${itemAmount + ' ' + itemWord}`)
-} else if (lastNumber >= 2 || lastNumber <= '4') {
+} else if (lastNumber >= 2 && lastNumber <= 4) {
     alert(`У вас в корзине ${itemAmount + ' ' + itemWord + 'а'}`)
-} else if (lastNumber <= 9 || lastNumber >= '5' || lastNumber === 0) {
+} else {
     alert(`У вас в корзине ${itemAmount + ' ' + itemWord  + 'ов'}`)
 }
