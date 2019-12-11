6112f4c17ba59bbef47646556beb3ed3173eb17a
julia <julianna.smirnova@gmail.com>
2019-12-11 12:47:50 +0200

    task 09, range fixed

--- smirnova_yuliia/smirnova_yuliia_hw_01/js/task_09.js
+++ smirnova_yuliia/smirnova_yuliia_hw_01/js/task_09.js
@@ -1,9 +1,9 @@
 let workExperience = prompt("Введите ваш стаж работы (кол-во лет)");
 if (workExperience >= 0 && workExperience <= 3) {
     alert("Ваша надбавка к зарплате 0%")
-} else if (workExperience >= 3 && workExperience <= 10) {
+} else if (workExperience >= 3 && workExperience < 10) {
     alert("Ваша надбавка к зарплате 10%")
-} else if (workExperience >= 10 && workExperience <= 20) {
+} else if (workExperience >= 10 && workExperience < 20) {
     alert("Ваша надбавка к зарплате 20%")
 } else if (workExperience >= 20 ) {
     alert("Ваша надбавка к зарплате 25%")
