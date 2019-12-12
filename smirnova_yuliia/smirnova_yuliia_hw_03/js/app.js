9b8ab755a74208830b0bf8032bc45684128be182
julia <julianna.smirnova@gmail.com>
2019-12-13 00:40:43 +0200

    task_03_02

--- smirnova_yuliia/smirnova_yuliia_hw_03/js/app.js
+++ smirnova_yuliia/smirnova_yuliia_hw_03/js/app.js
@@ -1,7 +1,13 @@
-// Task_01
+// Task_01 Вывести числа от 4 до 400 на экран.
 
-let num = 1;
-while (num <=400) {
-    console.log(num++);
+let num1 = 1;
+while (num1 <=400) {
+    console.log(num1++);
+}
+
+// Task_02 Вывести числа в последовательности: 4 7 10 13 с помощью цикла.
+
+for(let num2 = 4; num2 <= 13; num2 = num2 + 3) {
+    console.log(num2);
 }
 
