8e5e21171b37538c4b0be7f8b1a2c1393d13ffb1
Aleksei <weber.tuc@gmail.com>
2019-12-19 13:02:16 +0200

    Home_work1

---
+++ Home_Worke1/01/js/app.js
@@ -0,0 +1,13 @@
+const a = 40;
+const b = 90;
+let x;
+if(a>b) {
+    x=a+b/2*4;
+}
+if(a==b){
+    x=400;
+}
+if(a<b){
+    x=a-b+2/b*4
+}
+console.log(x);
---
+++ Home_Worke1/08/js/app.js
@@ -0,0 +1,8 @@
+let a = Number( prompt('введите число',));
+let date=new Date();
+date=date.getFullYear();
+if (date-a>=16){
+    alert('добро пожаловать')
+}else {
+    alert('вход восприщен')
+}
