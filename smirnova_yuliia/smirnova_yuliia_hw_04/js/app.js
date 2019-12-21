d8824a5984af887e5426c8f5823e4044bf07dfec
julia <julianna.smirnova@gmail.com>
2019-12-21 11:44:51 +0200

    tasks structure added

--- smirnova_yuliia/smirnova_yuliia_hw_04/js/app.js
+++ smirnova_yuliia/smirnova_yuliia_hw_04/js/app.js
@@ -1,3 +1,32 @@
 // Task 01
-
-
+console.log('--Task 01');
+// Task 02
+console.log('--Task 02');
+// Task 03
+console.log('--Task 03');
+// Task 04
+console.log('--Task 04');
+// Task 05
+console.log('--Task 05');
+// Task 06
+console.log('--Task 06');
+// Task 07
+console.log('--Task 07');
+// Task 08
+console.log('--Task 08');
+// Task 09
+console.log('--Task 09');
+// Task 10
+console.log('--Task 10');
+// Task 11
+console.log('--Task 11');
+// Task 12
+console.log('--Task 12');
+// Task 13
+console.log('--Task 13');
+// Task 14
+console.log('--Task 14');
+// Task 15
+console.log('--Task 15');
+// Task 16
+console.log('--Task 15');
