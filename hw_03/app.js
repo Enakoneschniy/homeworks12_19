dcc5a467250d6c11f6f49724523ecb627526f0c7
sergkapustnyk <serg.mrkrabs@gmail.com>
2019-12-22 22:34:32 +0200

    Homework_03 (Tasks 01-09)

---
+++ hw_03/app.js
@@ -0,0 +1,79 @@
+{console.log (`HW_03 Task01`)
+}
+{let number;
+number = 1;
+while (number <= 400) {
+    console.log (number++)
+}
+}
+{console.log (`HW_03 Task02`)
+}
+{
+    let number = 1;
+    while (number < 13) {
+        console.log(number += 3)
+    }
+}
+{console.log (`HW_03 Task03`)
+}
+{
+    let number = 654;
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
+{console.log (`HW_03 Task04`)
+}
+{
+    let number = 1983;
+    while (number <= 2017) {
+        console.log(number++)
+    }
+}
+{console.log (`HW_03 Task05`)
+}
+{
+    let number = -6;
+    do {
+        console.log(number +=2);
+    } while (number < 100);
+}
+{console.log (`HW_03 Task06`)
+}
+{
+    for (let number = 1; number <= 9; number++) {
+        console.log(7 * number)
+    }
+}
+{console.log (`HW_03 Task07`)
+}
+{
+    for (let number = 1000; number <= 2000; number++) {
+        console.log(String.fromCharCode(number))
+    }
+}
+{console.log (`HW_03 Task08`)
+}
+{
+    let firstNum = 0;
+    for (let number = 1; number <= 100; number++)
+        sum = firstNum += number;
+    console.log (sum);
+}
+{console.log (`HW_03 Task09`)
+}
+{
+    let firstNum = 1;
+    for (let number = 1; number < 50; number++)
+        mult = firstNum *= number;
+    console.log (mult);
+}
+{console.log (`HW_03 Task10`)
+}
+{
+
+}
+
+
+
+
